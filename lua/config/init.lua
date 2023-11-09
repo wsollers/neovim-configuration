c5ba155c62465af6f52739bf9c1615d27b4aa945
BoondockTaints <wsollers@gmail.com>
2023-11-09 07:30:22 -0500

    Current nvim config in lazy
    Current nvim config in lazy

---
+++ lua/config/init.lua
@@ -0,0 +1,50 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("config.options")
+require("config.globals")
+require("config.keymaps")
+require("config.autocmds")
+
+
+
+local plugins = "plugins"
+
+local opts = {
+	defaults = {
+		lazy = false,
+	},
+	install = {
+		colorscheme = { "nightfox" }
+		--colorscheme = { "gruvbox" },
+	},
+	rtp = {
+		disabled_plugins = {
+			"gzip",
+			"matchit",
+			"matchparen",
+			"netrw",
+			"netrwPlugin",
+			"tarPlugin",
+			"tohtml",
+			"tutor",
+			"zipPlugin",
+		},
+	},
+	change_detection = {
+		notify = true,
+	},
+}
+
+require("lazy").setup(plugins, opts)
+
---
+++ lua/plugins/toggleterm-nvim.lua
@@ -0,0 +1,6 @@
+return {
+  'akinsho/toggleterm.nvim', version = "*", config = true, lazy = false
+}
+
+  --{'akinsho/toggleterm.nvimddd', version = "*", opts = {--[[ things you want to change go here]]}
+
