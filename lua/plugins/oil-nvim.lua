1cb71501ab57e8a41150872b4da63ecb4d502b7e
BoondockTaints <wsollers@gmail.com>
2023-11-09 09:17:18 -0500

    update themes
    update themes

--- lua/plugins/oil-nvim.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/stevearc/oil.nvim
---
-return {}
---return {
---  'stevearc/oil.nvim',
---  opts = {},
---  -- Optional dependencies
---  dependencies = { "nvim-tree/nvim-web-devicons" },
---}
