1dc89ad974647e04d69f8e6cafef8ab6f49cbb5f
BoondockTaints <wsollers@gmail.com>
2023-11-24 19:44:21 -0500

    Better GoLang configuration
    better GoLang Configuration

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -110,6 +110,8 @@ local config = function()
 	lspconfig.gopls.setup({
 		settings = {
 			gopls = {
+				completeUnimported = true,
+				usePlaceholders = true,
 				analyses = {
 					unusedparams = true,
 				},
