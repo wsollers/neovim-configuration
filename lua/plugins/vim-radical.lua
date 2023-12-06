5378746c39b55ceee34b5f1f555b5fd680648e27
BoondockTaints <wsollers@gmail.com>
2023-12-06 13:59:03 -0500

    Surround and Radical hookup
    Surround and Radical hookup

---
+++ lua/plugins/vim-radical.lua
@@ -0,0 +1,8 @@
+--
+-- https://github.com/glts/vim-radical
+--
+
+return {
+	{ "glts/vim-magnum" },
+	{ "glts/vim-radical" },
+}
---
+++ lua/plugins/vim-surround.lua
@@ -0,0 +1,8 @@
+--
+-- Description: Surround text with quotes, brackets, etc.
+--  See:
+--  https://github.com/tpope/vim-surround
+return {
+	{ "tpope/vim-surround", opt = true },
+	{ "tpope/vim-repeat", opt = true },
+}
