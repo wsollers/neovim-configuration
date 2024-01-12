d8939816933fb6cecdfe1dede0e858267e3975e6
BoondockTaints <wsollers@gmail.com>
2024-01-12 09:33:30 -0500

    CLeanup

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,39 +1,68 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "leoluz/nvim-dap-go",
-    "theHamsta/nvim-dap-virtual-text",
-    "nvim-telescope/telescope-dap.nvim",
-  },
-  config = function()
-    local dap = require("dap") 
-    local dapui = require("dapui")
-    local dapgo = require("dap-go")
-    local dapvt = require("nvim-dap-virtual-text")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+		"theHamsta/nvim-dap-virtual-text",
+		"nvim-telescope/telescope-dap.nvim",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		local dapgo = require("dap-go")
+		local dapvt = require("nvim-dap-virtual-text")
 
-    require("dapui").setup()
-    require("dap-go").setup()
-    require("nvim-dap-virtual-text").setup()
+		require("dapui").setup()
+		require("dap-go").setup()
+		require("nvim-dap-virtual-text").setup()
 
-    require("telescope").load_extension("dap")
+		require("telescope").load_extension("dap")
 
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<leader>dc", dap.continue, {})
+		vim.keymap.set("n", "<leader>ds", dap.step_over, {})
+		vim.keymap.set("n", "<leader>di", dap.step_into, {})
+		vim.keymap.set("n", "<leader>do", dap.step_out, {})
 
-    vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint,  {})
-    vim.keymap.set("n", "<leader>dc", dap.continue, {})
-
-  end,
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			args = { "-i", "dap" },
+		}
+		dap.configurations.c = {
+			{
+				name = "Launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+			},
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+			}
+    }
+	end,
 }
