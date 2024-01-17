6f074785bcf26157afc3ad280175d2bd2f6f0f74
BoondockTaints <wsollers@gmail.com>
2024-01-17 11:08:58 -0500

    C,C++,Rust debug working
    C,C++, Rust debug working

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -37,32 +37,89 @@ return {
 		vim.keymap.set("n", "<leader>di", dap.step_into, {})
 		vim.keymap.set("n", "<leader>do", dap.step_out, {})
 
-		dap.adapters.gdb = {
+		--C/C++/Rust
+		local dap = require("dap")
+		dap.adapters.lldb = {
 			type = "executable",
-			command = "gdb",
-			args = { "-i", "dap" },
-		}
-		dap.configurations.c = {
-			{
-				name = "Launch",
-				type = "gdb",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				cwd = "${workspaceFolder}",
-			},
+			command = "/usr/bin/lldb-vscode-14", -- adjust as needed, must be absolute path
+			name = "lldb",
 		}
 		dap.configurations.cpp = {
 			{
 				name = "Launch",
-				type = "gdb",
+				type = "lldb",
 				request = "launch",
 				program = function()
 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 				end,
 				cwd = "${workspaceFolder}",
-			}
-    }
+				stopOnEntry = false,
+				args = {},
+				runInTerminal = false,
+			},
+		}
+		dap.configurations.c = dap.configurations.cpp
+		dap.configurations.rust = dap.configurations.cpp
+		--Bash
+		dap.adapters.bashdb = {
+			type = "executable",
+			command = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
+			name = "bashdb",
+		}
+		dap.configurations.sh = {
+			{
+				type = "bashdb",
+				request = "launch",
+				name = "Launch file",
+				showDebugOutput = true,
+				pathBashdb = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+				pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
+				trace = true,
+				file = "${file}",
+				program = "${file}",
+				cwd = "${workspaceFolder}",
+				pathCat = "cat",
+				pathBash = "/bin/bash",
+				pathMkfifo = "mkfifo",
+				pathPkill = "pkill",
+				args = {},
+				env = {},
+				terminalKind = "integrated",
+			},
+		}
+		--Go
+		dap.adapters.delve = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				command = "dlv",
+				args = { "dap", "-l", "127.0.0.1:${port}" },
+			},
+		}
+
+		-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+		dap.configurations.go = {
+			{
+				type = "delve",
+				name = "Debug",
+				request = "launch",
+				program = "${file}",
+			},
+			{
+				type = "delve",
+				name = "Debug test", -- configuration for debugging test files
+				request = "launch",
+				mode = "test",
+				program = "${file}",
+			},
+			-- works with go.mod packages and sub packages
+			{
+				type = "delve",
+				name = "Debug test (go.mod)",
+				request = "launch",
+				mode = "test",
+				program = "./${relativeFileDirname}",
+			},
+		}
 	end,
 }
