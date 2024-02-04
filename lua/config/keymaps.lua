dd2bb5e0abddc062bae26829f518f543ec19d287
BoondockTaints <wsollers@gmail.com>
2024-02-04 11:43:54 -0500

    Harpoon2 rework
    Harpoon 2 rework

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,3 +26,5 @@ vim.api.nvim_set_keymap( "n", "<space>fb", ":Telescope file_browser<CR>", { nore
 
 -- open file_browser with the path of the current buffer
 --vim.api.nvim_set_keymap( "n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
+
+
